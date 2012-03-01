820e1ce35caf3f1fbe7c17a0ff7907330461ecc6
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-03-01 19:00:32 +0100

    added class attributes to course module and course

--- src/main/java/berlin/reiche/scheduler/model/Course.java
+++ src/main/java/berlin/reiche/scheduler/model/Course.java
@@ -1,9 +1,26 @@
 package berlin.reiche.scheduler.model;
 
+import java.util.Map;
+
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+import com.google.code.morphia.annotations.Reference;
+
 /**
+ * Represents a course unit which belongs to a {@link CourseModule}.
+ * 
  * @author Konrad Reiche
- *
+ * 
  */
+@Entity("course")
 public class Course {
 
+	@Id
+	long id;
+	int duration;
+	int count;
+	String type;
+	
+	@Reference
+	Map<String, Integer> features;
 }
--- src/main/java/berlin/reiche/scheduler/model/CourseModule.java
+++ src/main/java/berlin/reiche/scheduler/model/CourseModule.java
@@ -1,5 +1,32 @@
 package berlin.reiche.scheduler.model;
 
+import java.util.Map;
+
+import com.google.code.morphia.annotations.Embedded;
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+
+/**
+ * A course module is a self-contained, formally structured learning experience.
+ * A course module has one or more courses which have to be undertaken in oder
+ * to complete this module.
+ * 
+ * @author Konrad Reiche
+ * 
+ */
+@Entity("course_module")
 public class CourseModule {
 
+	@Id
+	int id;
+	String name;
+	int credits;
+	String assessmentType;
+
+	/**
+	 * List of courses assigned to the module each mapped to the number how
+	 * often the same course is offered. 
+	 */
+	@Embedded
+	Map<Course, Integer> courses;
 }
