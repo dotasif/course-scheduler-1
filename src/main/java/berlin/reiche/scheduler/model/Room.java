91f5ce02101ce47e484ead800d905a2346078884
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-04-09 03:10:47 +0200

    added a model class for the room entity

---
+++ src/main/java/berlin/reiche/scheduler/model/Room.java
@@ -0,0 +1,51 @@
+package berlin.reiche.scheduler.model;
+
+import com.google.code.morphia.annotations.Entity;
+import com.google.code.morphia.annotations.Id;
+
+/**
+ * Represents a room to which courses can be scheduled.
+ * 
+ * @author Konrad Reiche
+ * 
+ */
+@Entity("room")
+public class Room {
+
+	@Id
+	String number;
+	String name;
+
+	/**
+	 * This constructor is used by Morphia via Java reflections.
+	 */
+	@SuppressWarnings("unused")
+	private Room() {
+
+	}
+
+	/**
+	 * Creates a new room by assigning the parameters directly
+	 * 
+	 * @param number
+	 *            the room number which uniquely identifies the room.
+	 * @param the
+	 *            name of the room, for instance used to identify the type of
+	 *            the room.
+	 */
+	public Room(String number, String name) {
+		super();
+		this.number = number;
+		this.name = name;
+	}
+
+	
+	public String getNumber() {
+		return number;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+}
