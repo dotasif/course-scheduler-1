d57d24a0f50f41ac8058dc9b27640a0a6787c9fb
Konrad Reiche <konrad.reiche@fu-berlin.de>
2012-04-20 12:21:50 +0200

    added ExecutorService for handling the scheduling tasks

--- src/main/java/berlin/reiche/scheduler/scheduler/ScheduleTask.java
+++ src/main/java/berlin/reiche/scheduler/scheduler/ScheduleTask.java
@@ -1,11 +1,25 @@
 package berlin.reiche.scheduler.scheduler;
 
+import java.util.List;
 import java.util.concurrent.FutureTask;
 
+import berlin.reiche.scheduler.model.CourseModule;
+import berlin.reiche.scheduler.model.Room;
+import berlin.reiche.scheduler.model.Timeframe;
+
+/**
+ * A {@link FutureTask} specialized for the course scheduling.
+ * 
+ * The task is based on the {@link GreedyAlgorithm}.
+ * 
+ * @author Konrad Reiche
+ * 
+ */
 public class ScheduleTask extends FutureTask<CourseSchedule> {
 
-    public ScheduleTask(Runnable runnable, CourseSchedule result) {
-        super(runnable, result);
+    public ScheduleTask(Timeframe timeframe, List<CourseModule> modules,
+            List<Room> rooms) {
+        super(new GreedyAlgorithm(timeframe, modules, rooms));
     }
 
 }
--- src/main/java/berlin/reiche/scheduler/scheduler/Scheduler.java
+++ src/main/java/berlin/reiche/scheduler/scheduler/Scheduler.java
@@ -1,5 +1,15 @@
 package berlin.reiche.scheduler.scheduler;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import berlin.reiche.scheduler.MongoDB;
+import berlin.reiche.scheduler.model.CourseModule;
+import berlin.reiche.scheduler.model.Room;
+import berlin.reiche.scheduler.model.Timeframe;
+
 /**
  * The schedule is the component which is used to control the schedule and
  * launch the scheduler tasks.
@@ -8,5 +18,34 @@ package berlin.reiche.scheduler.scheduler;
  * 
  */
 public class Scheduler {
+
+    private final ExecutorService exec;
+    private final Timeframe timeframe;
+
+    public Scheduler() {
+        exec = Executors.newSingleThreadExecutor();
+        timeframe = new Timeframe(5, 12); // TODO: Implement a servlet module
+                                          // for this
+    }
+
+    /**
+     * Schedules all available course modules on all available rooms.
+     */
+    public void schedule() {
+
+        // TODO: Encapsulate the parameters as algorithm
+        List<CourseModule> modules = MongoDB.getAll(CourseModule.class);
+        List<Room> rooms = MongoDB.getAll(Room.class);
+
+        ScheduleTask task = new ScheduleTask(timeframe, modules, rooms);
+        exec.submit(task);
+
+        try {
+            CourseSchedule result = task.get();
+            System.out.println(result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
